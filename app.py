7875ae4251aa9796d38b444214b5336f9c6484bd
ItzKeeni <maratkanovartem89@mail.ru>
2024-12-22 20:34:46 +0300

    refactoring

--- app.py
+++ app.py
@@ -1,21 +1,36 @@
 import subprocess
 import time
-from flask import Flask, jsonify, render_template, Response
+from flask import Flask, jsonify, render_template
 import psutil
 
 app = Flask(__name__)
 
+def get_system_stats():
+    # Получаем информацию о процессоре
+    cpu_percent = psutil.cpu_percent(interval=1)
+    temperature = get_cpu_temperature()
+    
+    # Получаем информацию о RAM
+    memory_info = psutil.virtual_memory()
+    ram_stats = {
+        'total': memory_info.total / (1024 ** 3),  # в Гб
+        'used': memory_info.used / (1024 ** 3),    # в Гб
+        'percent': memory_info.percent
+    }
+
+    return {
+        'cpu_percent': cpu_percent,
+        'cpu_temp': temperature,
+        'ram_stats': ram_stats
+    }
+
 def get_cpu_temperature():
     try:
-        # Выполняем команду `sensors` и получаем вывод
         output = subprocess.check_output(['sensors']).decode('utf-8')
-        # Ищем строку с температурой процессора
         for line in output.splitlines():
-            if 'Core' in line:  # Обычно температура процессора начинается со слова "Core"
-                # Извлекаем температуру из строки
+            if 'Core' in line:
                 parts = line.split()
-                # Предполагаем, что температура будет в формате 'xx.x°C'
-                temp = parts[2]  # Обычно температура находится на третьей позиции
+                temp = parts[2]
                 return temp
     except Exception as e:
         return str(e)
@@ -24,34 +39,20 @@ def get_cpu_temperature():
 def index():
     return render_template('index.html')
 
-@app.route("/cpu_percent", methods=["GET"])
-def cpu_percent():
-    return jsonify(cpu_percent=psutil.cpu_percent(interval=1))
-
-@app.route('/cpu_temperature', methods=['GET'])
-def cpu_temperature():
-    temperature = get_cpu_temperature()
-    return jsonify(cpu_temp=temperature)
-
-@app.route("/ram_stats", methods=["GET"])
-def ram_stats():
-    memory_info = psutil.virtual_memory()
-    total_memory_gb = memory_info.total / (1024 ** 3)
-    used_memory_gb = memory_info.used / (1024 ** 3)
-    memory_perecent = memory_info.percent
-    return jsonify(ram_use=f"{used_memory_gb:.2f} Гб / {total_memory_gb:.2f} Гб | {memory_perecent}%")
+@app.route("/system_stats", methods=["GET"])
+def system_stats():
+    stats = get_system_stats()
+    return jsonify(stats)
 
 @app.route("/network_speed", methods=["GET"])
 def network_speed():
     net_io_start = psutil.net_io_counters()
-    time.sleep(1)  # Ждем 1 секунду для измерения
+    time.sleep(1)
     net_io_end = psutil.net_io_counters()
 
-    # Вычисляем разницу в байтах
     bytes_sent = net_io_end.bytes_sent - net_io_start.bytes_sent
     bytes_recv = net_io_end.bytes_recv - net_io_start.bytes_recv
 
-    # Переводим байты в мегабиты и округляем до сотых
     mb_sent = round((bytes_sent * 8) / (1024 * 1024), 2)
     mb_recv = round((bytes_recv * 8) / (1024 * 1024), 2)
 
--- static/script.js
+++ static/script.js
@@ -1,44 +1,55 @@
-document.body.style.zoom = 0.25;
-console.log("wefwef")
+document.body.style.zoom = 0.25 
 
-function updateCpuPercent() {
-    fetch('/cpu_percent')
+function updateSystemStats() {
+    fetch('/system_stats')
         .then(response => response.json())
         .then(data => {
-            document.getElementById('cpu-percent').innerText = data.cpu_percent + '%';
-        })
-        .catch(error => console.error('Error fetching CPU percent:', error));
-}
+            const cpuPercent = data.cpu_percent 
+            const cpuTemp = parseFloat(data.cpu_temp)
+            const ramUsed = data.ram_stats.used 
+            const ramTotal = data.ram_stats.total 
+            const ramPercent = data.ram_stats.percent 
 
-function updateRamStats() {
-    fetch('/ram_stats')
-        .then(response => response.json())
-        .then(data => {
-            document.getElementById('ram').innerText = data.ram_use;
+            document.getElementById('cpu-percent').innerText = cpuPercent + '%' 
+            document.getElementById('cpu-temperature').innerText = cpuTemp 
+            document.getElementById('ram-used').innerText = ramUsed.toFixed(2) 
+            document.getElementById('ram-total').innerText = ramTotal.toFixed(2) 
+            document.getElementById('ram-percent').innerText = ramPercent 
+
+            const cpuTempElement = document.getElementById('cpu-temperature') 
+            const ramUsedElement = document.getElementById('ram-used') 
+            const cpuPercentElement = document.getElementById('cpu-percent') 
+
+            if (cpuTemp > 70) {
+                cpuTempElement.classList.add('alert') 
+            } else {
+                cpuTempElement.classList.remove('alert') 
+            }
+
+            if (ramPercent > 80) {
+                ramUsedElement.classList.add('alert') 
+            } else {
+                ramUsedElement.classList.remove('alert') 
+            }
+
+            if (cpuPercent > 80) {
+                cpuPercentElement.classList.add('alert') 
+            } else {
+                cpuPercentElement.classList.remove('alert') 
+            }
         })
-        .catch(error => console.error('Error fetching CPU percent:', error));
+        .catch(error => console.error('Error fetching system stats:', error)) 
 }
 
 function updateNetStats() {
     fetch('/network_speed')
         .then(response => response.json())
         .then(data => {
-            document.getElementById('sent').innerText = data.sent;
-            document.getElementById('recv').innerText = data.recv;
+            document.getElementById('sent').innerText = data.sent 
+            document.getElementById('recv').innerText = data.recv 
         })
-        .catch(error => console.error('Error fetching CPU percent:', error));
+        .catch(error => console.error('Error fetching network stats:', error)) 
 }
 
-function updateCpuTemperature() {
-    fetch('/cpu_temperature')
-        .then(response => response.json())
-        .then(data => {
-            document.getElementById('cpu-temperature').innerText = data.cpu_temp;
-        })
-        .catch(error => console.error('Error fetching CPU percent:', error));
-}
-
-setInterval(updateCpuPercent, 1000); // Обновлять каждую секунду
-setInterval(updateRamStats, 1000); // Обновлять каждую секунду
-setInterval(updateNetStats, 1000); // Обновлять каждую секунду
-setInterval(updateCpuTemperature, 1000); // Обновлять каждую секунду
+setInterval(updateSystemStats, 1000)
+setInterval(updateNetStats, 1000)
